ff8b8b1710628f9d0f30c91e7ad453ed55f45137
prostiak2019 <yarovenkodmv@gmail.com>
2020-08-05 12:59:01 +0300

    refactor: change structure of the project

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './a1-main/m1-ui/App';
 import * as serviceWorker from './serviceWorker';
+import {Provider} from 'react-redux';
+import store from "./a1-main/m2-bll/redux/redux-store";
+import {HashRouter} from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <Provider store={store}>
+        <App/>
+      </Provider>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
