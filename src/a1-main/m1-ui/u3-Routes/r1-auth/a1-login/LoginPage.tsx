6517ebe504b43b1b572b427130a3216f14962ec1
prostiak2019 <yarovenkodmv@gmail.com>
2020-08-21 19:47:17 +0300

    feat: add Login button

--- src/a1-main/m1-ui/u3-Routes/r1-auth/a1-login/LoginPage.tsx
+++ src/a1-main/m1-ui/u3-Routes/r1-auth/a1-login/LoginPage.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import {ButtonSelect} from '../../../u1-common/Button/Button';
 
 export const LoginPage = () => {
   return (
     <div>
       LoginPage
+      <ButtonSelect/>
     </div>
   )
 }
