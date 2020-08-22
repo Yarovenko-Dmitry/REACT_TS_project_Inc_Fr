09c3f8bca0fe2f623a6c55765ccae75dcebe6e57
prostiak2019 <yarovenkodmv@gmail.com>
2020-08-22 20:19:58 +0300

    Updates

---
+++ precache-manifest.d56973e9d611a917783c940a8b768afd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0baf9744333d090b0d3dd57e730d954b",
+    "url": "/REACT_TS_project_Inc_Fr/index.html"
+  },
+  {
+    "revision": "b4f933015f854fe9ef1a",
+    "url": "/REACT_TS_project_Inc_Fr/static/css/main.bbdc065a.chunk.css"
+  },
+  {
+    "revision": "31bb002da346a1483597",
+    "url": "/REACT_TS_project_Inc_Fr/static/js/2.4160281d.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/REACT_TS_project_Inc_Fr/static/js/2.4160281d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4f933015f854fe9ef1a",
+    "url": "/REACT_TS_project_Inc_Fr/static/js/main.2e4228f2.chunk.js"
+  },
+  {
+    "revision": "eac29524581489eed1d4",
+    "url": "/REACT_TS_project_Inc_Fr/static/js/runtime-main.90ce0e52.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/REACT_TS_project_Inc_Fr/precache-manifest.39e4824745b5a84f31e6dbe45cb4e6cd.js"
+  "/REACT_TS_project_Inc_Fr/precache-manifest.d56973e9d611a917783c940a8b768afd.js"
 );
 
 self.addEventListener('message', (event) => {
