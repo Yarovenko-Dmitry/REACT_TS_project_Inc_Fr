d00c79aea86c09904e5bb444dc4aec822d15253f
prostiak2019 <yarovenkodmv@gmail.com>
2020-08-26 22:06:30 +0300

    feat: add login formik

--- src/redux/login-reducer.ts
+++ src/redux/login-reducer.ts
@@ -1,13 +1,59 @@
-export type LoginReducerType = {};
+import {Dispatch} from 'redux';
+import {passwordRecovery} from '../api/api';
 
-let internalState: LoginReducerType = {};
 
-const loginReducer = (state: LoginReducerType = internalState, action: any) => {
+export type LoginReducerType = {
+  error: string
+  success: boolean
+  loading: boolean
+};
+
+let internalState: LoginReducerType = {
+  error: '',
+  success: false,
+  loading: false
+};
+
+const loginReducer = (state: LoginReducerType = internalState, action: ActionTypes) => {
 
   switch (action.type) {
+    case 'login/SET_SUCCESS':
+      return {
+        ...state, success: action.success
+      };
+    case 'login/SET_LOADING':
+      return {
+        ...state, loading: action.loading
+      };
+    case 'login/SET_ERROR':
+      return {
+        ...state, error: action.error
+      };
     default:
       return state;
   }
 }
+
+const setSuccessAC = (success: boolean) => ({
+  type: 'login/SET_SUCCESS',
+  success
+} as const);
+const setLoadingAC = (loading: boolean) => ({
+  type: 'login/SET_LOADING',
+  loading
+} as const);
+export const setErrorAC = (error: string) => ({
+  type: 'login/SET_ERROR',
+  error
+} as const);
+
+type ActionTypes =
+  | ReturnType<typeof setErrorAC>
+  | ReturnType<typeof setSuccessAC>
+  | ReturnType<typeof setLoadingAC>
+
+export const setLoginTC = (password: string, token: string) => (dispatch: Dispatch<ActionTypes>) => {
+
+};
 
 export default loginReducer
