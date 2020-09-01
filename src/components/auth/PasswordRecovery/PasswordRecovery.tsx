b6ca9ded57a762e27d117117fb51bf33e89dcd67
gmazur92 <gmazur92@gmail.com>
2020-09-01 17:23:49 +0300

    refactor: change page layout

--- src/components/auth/PasswordRecovery/PasswordRecovery.tsx
+++ src/components/auth/PasswordRecovery/PasswordRecovery.tsx
@@ -1,48 +1,129 @@
-import React from 'react';
-import style from './recoveryPage.module.css';
-import {useFormik} from 'formik';
+import React, {useEffect, useState} from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import EmailIcon from '@material-ui/icons/Email';
+import Typography from '@material-ui/core/Typography';
+import {makeStyles, withStyles} from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 import {useDispatch, useSelector} from 'react-redux';
-import {recoverPasswordTC} from '../../../redux/passwordRecovery-reducer';
 import {AppRootStateType} from '../../../redux/redux-store';
+import {recoverPasswordTC} from '../../../redux/passwordRecovery-reducer';
+import {useFormik} from 'formik';
+
+const useStyles = makeStyles((theme) => ({
+	paper: {
+		marginTop: theme.spacing(8),
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+	},
+	avatar: {
+		margin: theme.spacing(1),
+		backgroundColor: '#2c3258',
+	},
+	form: {
+		width: '100%',
+		marginTop: theme.spacing(3),
+	},
+	submit: {
+		margin: theme.spacing(3, 0, 2),
+		backgroundColor: '#e4b61a',
+		'&:hover': {
+			backgroundColor: '#2c3258'
+		}
+	},
+}));
+
+const ValidationTextField = withStyles({
+	root: {
+		'& input:valid + fieldset': {
+			borderColor: 'green',
+			borderWidth: 2,
+		},
+		'& input:valid:focus + fieldset': {
+			borderLeftWidth: 6,
+			padding: '4px !important',
+		},
+	},
+})(TextField);
 
 export const PasswordRecovery = () => {
 
+	const classes = useStyles();
 	const dispatch = useDispatch();
 
-	const error = useSelector<AppRootStateType, string>(state => state.pwRecoveryRequest.error);
-	const success = useSelector<AppRootStateType, boolean>(state => state.pwRecoveryRequest.success);
-	const loading = useSelector<AppRootStateType, boolean>(state => state.pwRecoveryRequest.loading);
-
 	const formik = useFormik({
+		validateOnBlur: true,
+		validateOnChange: false,
+		validate: (values) => {
+			if(!values.email) {
+				return {
+					email: 'Email is required'
+				}
+			}else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+				return {
+					email: 'Invalid email address'
+				}
+			}
+		},
 		initialValues: {
 			email: ''
 		},
 		onSubmit: values => {
 			const {email} = values;
 			dispatch(recoverPasswordTC(email.trim()));
-		}
+		},
 	});
 
+	const error = useSelector<AppRootStateType, string>(state => state.pwRecoveryRequest.error);
+	const success = useSelector<AppRootStateType, boolean>(state => state.pwRecoveryRequest.success);
+	const loading = useSelector<AppRootStateType, boolean>(state => state.pwRecoveryRequest.loading);
+
 	return (
-		<section>
-			<div className={style.container}>
-				<div className={style.wrapper}>
-					<div className={style.form}>
-						<h1>Forgot password?</h1>
-						<form onSubmit={formik.handleSubmit}>
-							<div className={style.error}>{error}</div>
-							{success &&
-                            <div className={style.success}>An email with further instructions has
-                                been sent to the address provided. Please check your email!</div>}
-							<input type="email" required placeholder="Enter your email"
-							       {...formik.getFieldProps('email')}
+		<Container component="main" maxWidth="xs">
+			<CssBaseline/>
+			<div className={classes.paper}>
+				<Avatar className={classes.avatar}>
+					<EmailIcon/>
+				</Avatar>
+				<Typography component="h1" variant="h5">
+					Reset password
+				</Typography>
+				<form className={classes.form} onSubmit={formik.handleSubmit}>
+					<Grid container spacing={2}>
+						<Grid item xs={12}>
+							{formik.errors.email ? <div>{formik.errors.email}</div> : null}
+							<ValidationTextField
+								error={!!formik.errors.email}
+								helperText={error || success}
+								label="Email Address"
+								fullWidth
+								required
+								variant="outlined"
+								name="email"
+								autoComplete="email"
+								id="validation-outlined-input"
+								{...formik.getFieldProps('email')}
 							/>
-							<button disabled={loading} className={style.btn}>Send</button>
-						</form>
-					</div>
-				</div>
+						</Grid>
+					</Grid>
+					<Button
+						type="submit"
+						fullWidth
+						variant="contained"
+						color="primary"
+						className={classes.submit}
+						disabled={loading}
+					>
+						Send
+					</Button>
+				</form>
 			</div>
-		</section>
+		</Container>
 	);
 };
 
+export default PasswordRecovery;
