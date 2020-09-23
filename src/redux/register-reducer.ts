1853417d85745e86f5cfc9e76403ab1664308d26
prostiak2019 <yarovenkodmv@gmail.com>
2020-09-23 18:58:42 +0300

    fix: redux-store.ts

--- src/redux/register-reducer.ts
+++ src/redux/register-reducer.ts
@@ -3,45 +3,42 @@ import {authAPI} from "../api/api";
 
 
 let internalState: RegisterReducerType = {
-    isRegistered: false
+  isRegistered: false
 };
 
 export const registerReducer = (state: RegisterReducerType = internalState, action: ActionsType) => {
 
-    switch (action.type) {
-        case "register/SET-IS-REGISTERED":
-            debugger
-            return {...state, isRegistered: action.value}
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case "register/SET-IS-REGISTERED":
+
+      return {...state, isRegistered: action.value}
+    default:
+      return state;
+  }
 }
 
 const setRegisterAC = (value: boolean) => ({
-    type: 'register/SET-IS-REGISTERED', value
+  type: 'register/SET-IS-REGISTERED', value
 })
 
 export const RegisterTC = (data: RegisterParamsType) => async (dispatch: Dispatch<ActionsType>) => {
-    debugger
-    try {
-        let response = await authAPI.register(data)
-        debugger
-            dispatch(setRegisterAC(true))
-    } catch (e) {
-        debugger
-        console.log(e)
-    }
+  try {
+    let response = await authAPI.register(data)
+    dispatch(setRegisterAC(true))
+  } catch (e) {
+    console.log(e)
+  }
 
 }
 
 //types
 type RegisterReducerType = {
-    isRegistered: boolean
+  isRegistered: boolean
 };
 type ActionsType = ReturnType<typeof setRegisterAC>
 type ThunkDispatch = Dispatch<ActionsType>
 
 type RegisterParamsType = {
-    email: string
-    password: string
+  email: string
+  password: string
 }
